918fee83a4d23b595c32dfe0279cf8a4fb34ce50
GeraltWitcher1 <denis22@inbox.lv>
2021-12-08 18:11:41 +0100

    Stashing changes

--- CarSharing-Client/Data/Impl/UserService.cs
+++ CarSharing-Client/Data/Impl/UserService.cs
@@ -9,7 +9,7 @@ namespace CarSharing_Client.Data.Impl
 {
     public class UserService : IUserService
     {
-        private const string Uri = "http://10.154.212.114:8080";
+        private const string Uri = "http://localhost:8080";
         private readonly HttpClient _client;
 
         public UserService()
@@ -38,7 +38,6 @@ namespace CarSharing_Client.Data.Impl
             );
 
             var responseMessage = await _client.PostAsync($"{Uri}/session", accountAsStringContent);
-
             
             if (!responseMessage.IsSuccessStatusCode)
             {
