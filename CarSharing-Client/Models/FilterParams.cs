3091858a34109db2ac20a9066a9965c914246ef6
GeraltWitcher1 <denis22@inbox.lv>
2021-12-12 14:55:08 +0100

    Implemented filtering for search

---
+++ CarSharing-Client/Models/FilterParams.cs
@@ -0,0 +1,10 @@
+﻿namespace CarSharing_Client.Models
+{
+    public class FilterParams
+    {
+        public string CarType { get; set; }
+        public string Transmission { get; set; }
+        public string FuelType { get; set; }
+        public int? NumberOfSeats { get; set; }
+    }
+}
